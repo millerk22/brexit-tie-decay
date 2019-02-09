5ba2de8978783cd8bd4d902163a717feed383cc3
hexie1995 <39319332+hexie1995@users.noreply.github.com>
2019-02-08 23:06:56 -0800

    networkx matrix and spectral clustering fun added

--- code/tie-decay-matrix.py
+++ code/tie-decay-matrix.py
@@ -1,8 +1,9 @@
 # import os
 import pickle
 import numpy as np
-# import networkx as nx
-
+import networkx as nx
+from sklearn.cluster import SpectralClustering
+from sklearn import metrics
 # os.chdir("/Users/qinyichen/Documents/brexit-tie-decay/Data/data_3_9jun")
 # data = pd.read_csv("edge_list_updated.csv")
 
@@ -35,7 +36,7 @@ def tie_decay_matrix(edge_list, T, t_start=0, dt=60, alpha=0.1):
 
     # Update B at different time
     for t in range(t_start, T, dt):
-        print ("start calculating B at t =", t)
+#        print ("start calculating B at t =", t)
 
         # entries of B decay from t to t+dt
         B.update((k, v*np.exp(-alpha * dt)) for k, v in B.items())
@@ -53,5 +54,42 @@ def tie_decay_matrix(edge_list, T, t_start=0, dt=60, alpha=0.1):
 
     return B
 
-tie_decay_result = tie_decay_matrix(data, 1000)
-# result = tie_decay_matrix(data, 3425100)
+def get_tie_decay_matrix_inNetworkX(B):
+    """
+    INPUT: edge list in a format of dictionary
+    OUTPUT: a networkx graph object
+    """
+    # get a list from the dict
+    edglist = [(k[0], k[1], v) for k,v in B.items()]
+    FG=nx.Graph()
+    FG.add_weighted_edges_from(edglist)
+    return FG
+
+#def draw_tie_decay_matrix(M):
+    """
+    A function that does not really need to exist
+    """
+#    nx.draw(M)
+    
+def spectral_clustering_for_TD(G):
+    # get the largest connected component
+    H = G.subgraph(list(max(nx.connected_component_subgraphs(G), key=len)))
+    adj_mat = nx.to_numpy_matrix(H)
+    
+    # run spectral clustering with cluster size of 2
+    sc = SpectralClustering(2, affinity='precomputed', n_init=100)
+    sc.fit(adj_mat)
+    
+    print('spectral clustering')
+    print(sc.labels_)
+    return sc.labels_, H
+
+tie_decay_result = tie_decay_matrix(data, 10000)
+GC=get_tie_decay_matrix_inNetworkX(tie_decay_result)
+#draw_tie_decay_matrix(GC)
+label,H =spectral_clustering_for_TD(GC)
+#result = tie_decay_matrix(data, 3425100)
+
+
+
+
